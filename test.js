b44c97889c85fa96eef565e57093203a67335b8b
George Cairns <gee@Georges-Mac.local>
2021-01-26 00:14:28 +0000

    hash function so we dont always have to re-compute

--- test.js
+++ test.js
@@ -1,11 +1,22 @@
 const axios = require('axios').default;
 const util = require('util')
 const _ = require('lodash')
-const datefns = require('date-fns')
+const date = require('date-fns')
+const chalk = require("chalk")
+const fs = require('fs')
+const FormData = require('form-data');
+const request = require("request")
+
+const crypto = require('crypto');
+
+const fsWriteFile = util.promisify(fs.writeFile);
+const fsReadFile = util.promisify(fs.readFile);
+
+const { createCanvas, loadImage } = require('canvas')
 
 let log = console.log;
-// letgit push  echo = (x) => {console.log(util.inspect(x))}
 let echo = (x) => {console.log(JSON.stringify(x,null,2))}
+let title = (x) => {console.log(chalk.red(x + "\n---------------------------------------------------------------------"))}
 
 
 if (!("TRELLO_TOKEN" in process.env)) {
@@ -21,32 +32,287 @@ if (!("TRELLO_KEY" in process.env)) {
 let url = "https://api.trello.com/1"
 let boardId = "n6VBFMpa"
 
+
 let queryParams = {
 	key: process.env.TRELLO_KEY,
 	token: process.env.TRELLO_TOKEN
 }
 
-// let queryString = `key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`
+let queryString = `key=${process.env.TRELLO_KEY}&token=${process.env.TRELLO_TOKEN}`
 
+
+let projectNameForName = (cardName) => {
+		let matches = cardName.match(/^((p|P)roject|PROJECT) - (.+)/) 
+
+		if (matches) {
+			return {
+				replaced: true,
+				name: matches[3]
+			}
+		} else {
+			return {
+				replaced: false,
+				name: cardName
+			}
+		}
+}
+
+let readHash = async (file) => {
+	return new Promise((resolve,reject)=>{
+
+		fs.stat(file, async function(err, stat) {
+		    if(err == null) {
+		        let data = await fsReadFile(file, "utf8")
+				resolve(data)
+
+		    } else if(err.code === 'ENOENT') {
+		        resolve(0)
+		    } else {
+		        reject('Error opening hash file: ', err.code);
+		    }
+		});
+
+	})
+}
 
 async function main(){
+	return new Promise( async (resolve,reject)=>{
+		try {
 
-	//Get Pertinent trello data in a clean intermediate format
-	let cardData = await getAllBoardData(boardId)
-	let lists = await getLists()
-	let listsAndCards = await addCards(lists, cardData, ["Project Start Date", "SC","Not SC Eligible", "Skills"])
-	echo(listsAndCards)
+			let formatDescription = (desc) => {return "```\n" + JSON.stringify(desc,null,2) + "\n```"}
+			let dateString = date.format(new Date(), 'yyyy-MM-dd-h:mm:ss')
 
-	// Show totals
-	let outputReport = await makeBillingReport(listsAndCards)
-	echo(outputReport)
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			//Get Pertinent trello data in a clean intermediate format
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			let cardData = await getAllBoardData(boardId)
+			let lists = await getLists()
+			let listsAndCards = await addCards(lists, cardData, ["Project Start Date", "SC","Not SC Eligible", "Skills"])
 
-	// Get Engineer project move history 
-	let moveActions = await getAllMoveActions(boardId)
-	let moveList = await getMoveListForCard("6008456fcc5b0626995685c1",moveActions,lists)
+			let listsAndCards2 = listsAndCards.map(l => {
+				let result = projectNameForName(l.name)
+				l.projectName = result.name
+				l.project = result.replaced
+				return l
+			})
 
-	echo(moveList)
+			title("cleaned data")
+			echo(listsAndCards2)
 
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			// Billing Report
+			//-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			let billingReportCardId = 'D5KmQ4Da'
+			
+			let outputReport = await makeBillingReport(listsAndCards)
+			title("Billing Report")
+			echo(outputReport)
+
+			let reportHash = crypto.createHash('md5').update(JSON.stringify(outputReport)).digest('hex')
+			
+			if (reportHash == await readHash("/tmp/billingReport.txt")) {
+				console.log("Billing report: the hash is the same so don't re-update the board")
+			} else {
+				console.log("Billing report: re-update the board")
+				put(`cards/${billingReportCardId}`,{},{
+					desc: formatDescription(outputReport)
+				})
+				let imageLocation = createImageFile(
+					billingTextFn(
+						outputReport.totals.billing,
+						outputReport.totals.nonBilling,
+						outputReport.totals.pendingStartDate
+					),
+					"billing-" + dateString 
+				)[0]
+
+				await uploadAttachment(imageLocation, billingReportCardId)
+				await fsWriteFile("/tmp/billingReport.txt", reportHash, "utf8")
+			}
+
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			// Move report
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			let moveReportCardId = 'B2RtCsvU'
+
+			let manualMoveReports = await getManualMoveActions(cardData)
+			title("Manual Moves this week")
+			echo(manualMoveReports)
+
+			reportHash = crypto.createHash('md5').update(JSON.stringify(manualMoveReports)).digest('hex')
+			
+			if (reportHash == await readHash("/tmp/moveReport.txt")) {
+				console.log("Move report: the hash is the same so don't re-update the board")
+			} else {
+				console.log("Move report: re-update the board")
+				put(`cards/${moveReportCardId}`,{},{desc: formatDescription(manualMoveReports)})
+
+				let moveReportImageLocation = createImageFile(
+					movesTextFn(manualMoveReports),
+					"moves-" + dateString,
+					2000,
+					20 + (200 * manualMoveReports.length)
+				)[0]
+
+				await uploadAttachment(moveReportImageLocation, moveReportCardId)
+				await fsWriteFile("/tmp/moveReport.txt", reportHash, "utf8")
+			}
+
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			// Starter report
+			// -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+			let starterReportCardId = 'hi2gyC87'
+
+			let starterReport = await getStarterReport(listsAndCards)
+			title ("Starters next week")
+			echo(starterReport)
+
+			reportHash = crypto.createHash('md5').update(JSON.stringify(starterReport)).digest('hex')
+
+			if (reportHash == await readHash("/tmp/starterReport.txt")) {
+				console.log("Starter Report: the hash is the same so don't re-update the board")
+			} else {
+				console.log("Starter report: re-update the board")
+				put(`cards/${starterReportCardId}`,{},{desc: formatDescription(starterReport)})
+
+				let starterReportImageLocation = createImageFile(
+					starterTextFn(starterReport),
+					"starters-" + dateString,
+					2000,
+					20 + (200 * starterReport.length)
+				)[0]
+
+				await uploadAttachment(starterReportImageLocation, starterReportCardId)
+				await fsWriteFile("/tmp/starterReport.txt", reportHash, "utf8")
+
+			}
+
+			resolve("done")
+
+		} catch (e) {
+			reject (e)
+		}
+	})
+
+}
+
+let uploadAttachment = async (imageLocation, cardId) => {
+	return new Promise((resolve, reject) => {
+		try {
+
+			const formData = new FormData()
+
+			formData.append("key", queryParams.key);
+			formData.append("token", queryParams.token);
+			formData.append("file", fs.createReadStream(imageLocation));
+
+			var requestObj = request.post(url + '/cards/' + cardId + '/attachments', attachmentCallback);
+			requestObj._form = formData;
+
+			function attachmentCallback(err, httpResponse, body) {
+				if (httpResponse.statusCode == 200) {
+					resolve("successfully attached the file " );
+				} else {
+					reject('Could not attach the file to card:', httpResponse.statusMessage);
+				}
+			}
+
+		}
+		catch(e){
+			reject("error uploading doc:" + e)
+		}
+	})
+}
+
+let starterTextFn = (starters) => {
+	return (rect, text )=>{
+		let i = 20
+		starters.forEach(move => {
+			text(`${move.name}:`,	i,	 10, "#22A", '60pt Menlo')
+			text(` --> ${move.to}`,	i,	 600, "#491", '60pt Menlo')
+			i += 200
+		})
+	}
+}
+
+let movesTextFn = (moves) => {
+	return (rect, text )=>{
+		let i = 20
+		moves.forEach(move => {
+			text(`${move.name}:`,							i,	 10, "#22A", '60pt Menlo')
+			text(`${move.move.from} --> ${move.move.to}`,	i,	 600, "#A2A", '60pt Menlo')
+			i += 200
+		})
+	}
+}
+
+let billingTextFn = (billing = 0, nonBilling = 0, pending = 0) => {
+	return (rect, text )=>{
+
+		text("Billing: " + billing, 				0,	 10, "#2A2", '60pt Menlo')
+		text("Not Billing: " + nonBilling,			200, 10, "#F00", '60pt Menlo')
+		text("(Pending Start: " + pending + ")" ,	400, 10, "#22A", '60pt Menlo')
+	}
+}
+
+let createImageFile = (drawFn, outputFileName, width = 1200, height = 580)=>{
+
+	const padding = 30
+	const canvas = createCanvas(width, height)
+	const context = canvas.getContext('2d')
+
+	//Set background
+	context.fillStyle = "#FFF"
+	context.fillRect(0,0,width,height)
+
+	let rect = (top, height, color = '#3A3') => {
+		context.fillStyle = color
+		context.fillRect(padding, top + padding, width - (padding * 2), height - (padding * 2))
+	}
+
+	let text = (text, top = 0, left = 0, color = '#EEE', font  = '100pt Menlo') => {
+		// log("WOOOOOO: "  + left)
+		let myHeight = context.measureText(text).height
+		context.fillStyle = color
+		context.font = font
+		context.fillText(text, left + padding, top + padding + 70)
+	}
+	//Main pane
+	rect(0,height,"#FFE")
+
+	drawFn(rect, text)
+
+	const buffer = canvas.toBuffer('image/png')
+	let fileName = `./generated-report-${outputFileName}.png` 
+	fs.writeFileSync(fileName, buffer)
+	return [fileName, buffer]
+
+}
+
+
+let getStarterReport = async(listsAndCards)=>{
+	let starters = []
+
+	listsAndCards.forEach((list)=>{
+		list.cards.forEach((card)=>{
+			if (card["customFieldItems"]) {
+				card.customFieldItems.forEach((field)=>{
+					if (field.name == "Project Start Date") {
+						let projectStartDate = Date.parse(field.value.date)
+						if (date.compareAsc(Date.now(), projectStartDate) == -1) {
+							starters.push({
+								name: card.name,
+								to: projectNameForName(list.name).name,
+								date: field.value.date
+							})
+						} 
+					} 
+				})
+			}
+		})
+	})
+
+	return starters
 
 }
 
@@ -62,7 +328,7 @@ let getAllBoardData = async (boardId) => {
 let getAllMoveActions = async (boardId, cardData) => {
 	let params = {
 		filter: "updateCard",
-		since: datefns.subDays(Date.now(),7)
+		since: date.subDays(Date.now(),7)
 	}
 	let response = await get(`boards/${boardId}/actions`, params)
 
@@ -74,7 +340,28 @@ let getAllMoveActions = async (boardId, cardData) => {
 
 }
 
-let getMoveListForCard = async (cardId, moveActions, lists) => {
+let getManualMoveActions = async (cardData) => {
+
+	// Get Engineer project move history 
+	let moveActions = await getAllMoveActions(boardId)
+
+	let moveReports = await Promise.all(cardData.map(async (card)=>{
+		let moveList = await getMoveListForCard(card.id, moveActions)
+		return {
+			// moveList : moveList,
+			move: moveList[0],
+			id: card.id,
+			name: card.name
+		}
+	}))
+
+	return _.filter(moveReports, (moveReport)=>{
+		return (moveReport.move) 
+	})
+
+}
+
+let getMoveListForCard = (cardId, moveActions) => {
 
 	let cards = _.filter(moveActions,(action)=>{
 		return (action.data.card.id == cardId)
@@ -82,9 +369,9 @@ let getMoveListForCard = async (cardId, moveActions, lists) => {
 
 	let moves = _.map(cards,(card)=>{
 		return {
-			from: card.data.listBefore.name,
-			to: card.data.listAfter.name,
-			date: Date.parse(card.date)
+			from: projectNameForName(card.data.listBefore.name).name,
+			to: projectNameForName(card.data.listAfter.name).name,
+			date: new Date(card.date).toISOString()
 		}
 	})
 
@@ -95,7 +382,8 @@ let getMoveListForCard = async (cardId, moveActions, lists) => {
 let get = async (path, extraParams) => {
 
 	try {
-		return await axios.get(`${url}/${path}`,
+		return await axios.get(
+			`${url}/${path}`,
 			{
 				params: {
 					...queryParams, 
@@ -109,8 +397,43 @@ let get = async (path, extraParams) => {
 	}
 }
 
+let put = async (path, data, extraParams) => {
 
-// curl https://api.trello.com/1/boards/5a00adcebe1991022b4a4bb4/cards/?fields=name&customFieldItems=true&key={APIKey}&token={APIToken}
+	try {
+		return await axios.put(
+			`${url}/${path}`,
+			data,
+			{
+				params: {
+					...queryParams, 
+					...extraParams
+				}
+			}
+		)
+	}
+	catch (e){
+		log("error making put request: " +e)
+	}
+}
+
+let post = async (path, data, extraParams) => {
+
+	try {
+		return await axios.post(
+			`${url}/${path}`,
+			data,
+			{
+				params: {
+					...queryParams, 
+					...extraParams
+				}
+			}
+		)
+	}
+	catch (e){
+		log("error making post request: " +e)
+	}
+}
 
 let getCustomFieldDefinitionFromName = async (customFieldName) => {
 	let response = await get(`boards/${boardId}/customFields`)
@@ -118,7 +441,6 @@ let getCustomFieldDefinitionFromName = async (customFieldName) => {
 	let projectStartDateField = response.data.filter(field => {return (field.name == customFieldName)})
 	
 	return projectStartDateField[0]
-	// echo(projectStartDateField)
 
 }
 
@@ -151,7 +473,6 @@ let addCards = async (lists, allCards, customFieldNames)=> {
 					delete card.labels
 				}
 
-		
 				//For each user supplied custom field name, filter for that field only 
 				let customFieldItems = customFieldNames.reduce( (acc,targetFieldName)=> {
 
@@ -201,11 +522,10 @@ let makeBillingReport = async (lists) => {
 	}
 
 	lists.forEach((list)=>{
-		let matches = list.name.match(/^((p|P)roject|PROJECT) - (.+)/) 
+		if (list["project"]) {
 
-		if (matches) {
 			let projectTotals = {
-				project: matches[3],
+				project: list.projectName,
 				consultants: {
 					placed: 0,
 					billing: 0,
@@ -234,7 +554,7 @@ let makeBillingReport = async (lists) => {
 					card.customFieldItems.forEach((field)=>{
 						if (field.name == "Project Start Date") {
 							let projectStartDate = Date.parse(field.value.date)
-							if (datefns.compareAsc(Date.now(), projectStartDate) == -1) {
+							if (date.compareAsc(Date.now(), projectStartDate) == -1) {
 								billing = false
 								projectTotals.consultants.pendingStartDate++
 							} 
@@ -272,10 +592,27 @@ let makeBillingReport = async (lists) => {
 	return report
 }
 
+const isGoogleCloudEnv = !!process.env.GCP_PROJECT 
 
-// Start
-(async function() {
-	await main();
-})();
+if (!isGoogleCloudEnv) {
+	// Start
+	(async function() {
+		await main();
+	})();
+}
+
+exports.createReport = (req, res) => {
+  main()
+  .then(()=>{
+    let message = req.query.message || req.body.message || 'done.';
+  	res.status(200).send(message);
+  })
+  .catch((e)=>{
+  	res.status(500).send(e);
+  })
+
+};
+
+
 
 
